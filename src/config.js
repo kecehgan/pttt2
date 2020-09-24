6f71abeea20845835da4910d4b55a3a6687b8951
root <30491811+kecehgan@users.noreply.github.com>
2020-09-24 20:11:45 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {telegraf_token:'1238265066:AAF-VZ47nQeSJg6keRqjLiDU-5j6vN54fPg',
-database:'mongodb://localhost:27017',
+database:'mongodb://localhost/test',
 secret:'yoursecret'};
 
